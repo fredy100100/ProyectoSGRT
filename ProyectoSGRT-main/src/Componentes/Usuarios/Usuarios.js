015a054ab3c8e2da221bc0ae670b2b49dfaa38eb
Cristian Mendoza <cristian.mendozapds@gmail.com>
2023-12-01 20:33:27 -0500

    Segundo commit

--- ProyectoSGRT-main/src/Componentes/Usuarios/Usuarios.js
+++ ProyectoSGRT-main/src/Componentes/Usuarios/Usuarios.js
@@ -5,7 +5,7 @@ export const Usuarios = () =>{
     return(
     
      <div className="User">
-        <h1 className="text-center">AQUI SE DEBEN VISUALIZAR LOS USUARIOS</h1>
+        <h1 className="text-center">AQUI SE DEBEN VISUALIZAR LOS USUARIOS XD</h1>
       
 <Main/>
 <Section/>
