a0344445d71cdc98774d10fb55d375938949b5c8
Cristian Mendoza <cristian.mendozapds@gmail.com>
2023-12-01 21:29:46 -0500

    Se duplico el proyecto

--- ProyectoSGRT-main/src/Componentes/Home/UserProfile.js
+++
@@ -1,9 +0,0 @@
-import '../../App.css'
-export const UserProfile = () => {
-    return (
-      <div className="perfil">
-      <p>Nombre: Fredy Ruiz</p>
-      <p>Correo electrónico: fredyandresruiz9@gmail.com</p>
-    </div>
-    );
-  }
