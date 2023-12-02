a216fe7616ff0dffa9d3e2c18d2791d9679688e8
fredy100100 <fredyandresruiz9@gmail.com>
2023-12-01 21:01:01 -0500

    Se agrega inicio de sesion

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Home from './Componentes/Home/Home'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Estadisticas } from './Componentes/Estadisticas/Estadisticas';
+import { Inicio } from './Componentes/Inicio';
 
 
 function App() {
@@ -14,6 +15,8 @@ function App() {
   <Routes>
     <Route path='/Estadisticas' element={<Estadisticas/> }></Route>
     <Route path='/' element={<Home/>}></Route>
+    <Route path='/Inicio' element={<Inicio/>}></Route>
+
   </Routes>
 </Router>
 
