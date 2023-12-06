d0b3081eef7a1a7cf65ada47e7dbd700d9ff12f2
Cristian Mendoza <cristian.mendozapds@gmail.com>
2023-12-06 13:59:58 -0500

    Prueba

--- src/Componentes/Estadisticas/Estadisticas.js
+++ src/Componentes/Estadisticas/Estadisticas.js
@@ -1,16 +1,12 @@
-import { Main } from "../Main"
-import {Section} from '../Section';
-import '../../App.css'
-export const Estadisticas = () =>{
-    return(
-     <div className="Est">
-        <h1 className="text-center">AQUI SE DEBEN VISUALIZAR LAS ESTADISTICA</h1>
-      
-<Main/>
-<Section/>
-
-     </div>
-      
-
-    )
-}
+import { Main } from "../Main";
+import { Section } from "../Section";
+import "../../App.css";
+export const Estadisticas = () => {
+  return (
+    <div className="Est">
+      <h1 className="text-center">AQUI SE DEBEN VISUALIZAR LAS ESTADISTICA</h1>
+      <Main />
+      <Section />
+    </div>
+  );
+};
