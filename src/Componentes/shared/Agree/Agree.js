89e22364f0a2990922564775ecd1125897ec5e4d
Cristian Mendoza <cristian.mendozapds@gmail.com>
2023-12-06 16:41:24 -0500

    Link a Formulario Crear usuarios

--- src/Componentes/shared/Agree/Agree.js
+++ src/Componentes/shared/Agree/Agree.js
@@ -1,11 +1,14 @@
 import Button from "react-bootstrap/Button";
-import '../Agree/agree.css'
+import '../Agree/agree.css';
+import { Link } from "react-router-dom";
 
 export const Agree = () => {
   return (
     <div className="Agree-container">
       <div>
-        <Button variant="btn btn-light">Agregar Usuario</Button>
+        <Link to="../CrearUsuario">
+          <Button variant="btn btn-light">Agregar Usuario</Button>
+        </Link>        
       </div>
       <div>
         <Button variant="btn btn-light">Modificar Usuario</Button>
