29661789301d7be182488f3bd6465750130b890c
Cristian Mendoza <cristian.mendozapds@gmail.com>
2023-12-06 14:08:14 -0500

    Apps 2 solucion

--- src/App.js
+++ src/App.js
@@ -1,29 +1,26 @@
-import './App.css';
-import Home from './Componentes/Home/Home'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Estadisticas } from './Componentes/Estadisticas/Estadisticas';
-import { Inicio } from './Componentes/Inicio';
-import { Usuarios } from './Componentes/Usuarios/Usuarios';
-
+import "./App.css";
+import Home from "./Componentes/Home/Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Estadisticas } from "./Componentes/Estadisticas/Estadisticas";
+import { Inicio } from "./Componentes/Inicio";
+import { CreaciondeUsuarios } from "./Componentes/CreaciondeUsuarios";
+import { Inventario } from "./Componentes/Inventario";
+import { Usuarios } from "./Componentes/Usuarios/Usuarios";
 
 function App() {
   return (
     <div className="App">
-
-
-      
-
+      <Router>
         <Routes>
-          <Route path='/Estadisticas' element={<Estadisticas />}></Route>
-          <Route path='/' element={<Home />}></Route>
-          <Route path='/Inicio' element={<Inicio />}></Route>
-          <Route path='/Usuarios' element={<Usuarios />}></Route>
+          <Route path="/Estadisticas" element={<Estadisticas />}></Route>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/Inicio" element={<Inicio />}></Route>
+          <Route path="/CrearUsuario" element={<CreaciondeUsuarios />}></Route>
+          <Route path="/Inventario" element={<Inventario />}></Route>
+          <Route path="/Usuarios" element={<Usuarios />}></Route>
         </Routes>
-    
-
-
+      </Router>
     </div>
-
   );
 }
 
