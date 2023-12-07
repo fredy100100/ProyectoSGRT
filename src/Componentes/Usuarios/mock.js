3b2416902998098f4f3f05657383b4c4a0424707
Cristian Mendoza <cristian.mendozapds@gmail.com>
2023-12-06 20:01:32 -0500

    Tipo de dato actualizado

--- src/Componentes/Usuarios/mock.js
+++ src/Componentes/Usuarios/mock.js
@@ -3,13 +3,13 @@ export const mock = [
       nombre: 'Daniel',
       apellido: 'Mendoza',
       correo: 'daniel@gmail.com', 
-      celular: 3215343135
+      celular: '3215343135'
     },
     {
         nombre: 'Miguel',
         apellido: 'Cifuentes',
         correo: 'miguel@gmail.com',
-        celular: 3215343135
+        celular: '3215343135'
     },
     {
       nombre: 'Juan',
