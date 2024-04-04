432d99fb25bf0cc6d5a8ccb7b3ec4afc87f49646
DanielMen97 <133619338+DanielMen97@users.noreply.github.com>
2024-04-03 21:20:38 -0500

    Ajustes para el pullrequest

--- src/Componentes/InventarioX/Inventario.js
+++ src/Componentes/InventarioX/Inventario.js
@@ -1,12 +1,26 @@
+import { Main } from "../Main";
 import { Section } from "../Section";
 import { headTableActivos } from "./mock";
 import data from "./MOCK_DATA.json";
+import { Header } from "../shared/Header/Header"
 import { CrearActivo } from "./crearactivo/crearactivo";
 import "./inventario.css";
 import { SimpleTable } from "../shared/simpleTable/simpleTable";
 import { InputTable } from '../shared/inputTable/inputTable';
+import { BotonesNav } from "../shared/tablenavegation/botonesNav";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 export const Inventario = () => {
+
+  const [listActivos, setlistActivos]=useState([])
+  useEffect(() =>{
+    const activos=async () => {
+      const infoactivos=await axios.get("http://localhost:8080/acts")
+      const info=infoactivos.data;
+      setlistActivos(info)
+  }
+  activos()},[])
 
   return (
     <div className="Inv">
@@ -20,7 +34,7 @@ export const Inventario = () => {
 
         </div>
         <div className="table-content">
-          <SimpleTable filtro={<InputTable />} columns={headTableActivos} data={data} titulo="Activos" >
+          <SimpleTable filtro={<InputTable />} navegacion={<BotonesNav />} columns={headTableActivos} data={listActivos} titulo="Activos" >
           </SimpleTable>
         </div>
       </div>
